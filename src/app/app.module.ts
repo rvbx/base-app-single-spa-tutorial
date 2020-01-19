f64fa6cc53abca9d72a8b8bdcf49573522c2f4bb
Rodolfo Villas Boas <rodolfovillasboas@gmail.com>
2020-01-19 20:31:34 -0300

    Create micro app component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,12 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { MicroAppComponent } from './micro-app/micro-app.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MicroAppComponent
   ],
   imports: [
     BrowserModule
