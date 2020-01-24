def687ef39b341422a38af26e5e8489d2e25d6a0
Rodolfo Villas Boas <rodolfovillasboas@gmail.com>
2020-01-24 08:51:27 -0300

    Function created to register the child single-spa applications in the APP and start the single-spa

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,8 @@
 import { Component } from '@angular/core';
+import { MICRO_APPS } from 'src/environments/environment';
+
+// Single SPA
+import * as singleSpa from 'single-spa';
 
 @Component({
   selector: 'base-root',
@@ -7,4 +11,24 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'base-app';
+
+  ngAfterViewInit() {
+    /**
+     * Registra os micro apps
+     */
+    MICRO_APPS.forEach(MICRO_APP => {
+      console.log('MICRO_APP', MICRO_APP);
+      singleSpa.registerApplication(
+        MICRO_APP.appName,
+        // @ts-ignore
+        () => window.System.import(MICRO_APP.main).then(appModule => {
+          console.log('app module: single-spa', appModule);
+          return appModule.default;
+        }),
+        location => location.pathname.startsWith(MICRO_APP.path)
+      );
+    });
+
+    singleSpa.start();
+  }
 }
